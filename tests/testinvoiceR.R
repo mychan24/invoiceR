2f9a19f0f598ecf53425d105308ac8e5b2f6821e
Micaela Chan <4413270+mychan24@users.noreply.github.com>
2021-02-13 13:39:16 -0500

    Add packages to travis.yml and DESCRIPTION; remove old comment from testinvoiceR`

--- tests/testinvoiceR.R
+++ tests/testinvoiceR.R
@@ -1,17 +1,2 @@
-# invoiceR(invoice_yaml = file.path(ddir,"demo/invoice_demo.yaml"),
-#          client_yaml = file.path(ddir,"demo/client.yaml"),
-#          provider_yaml = file.path(ddir,"demo/provider.yaml"),
-#          tempalte_rmd = file.path(ddir, "templates/invoice_template.Rmd"),
-#          invdir = file.path(ddir, "demo"),
-#          income_file = file.path(ddir, "demo/new_income.csv")) 
 
 rmarkdown::render("./demo/ClientName_InvoiceGeneration.Rmd")
-
-# ddir <- "."
-# 
-# invoiceR(invoice_yaml = file.path(ddir,"demo/invoice_demo.yaml"),
-#          client_yaml = file.path(ddir,"demo/client.yaml"),
-#          provider_yaml = file.path(ddir,"demo/provider.yaml"),
-#          tempalte_rmd = file.path(ddir, "templates/invoice_template.Rmd"),
-#          invdir = "~/",
-#          income_file = file.path(ddir, "demo/new_income.csv")) 
